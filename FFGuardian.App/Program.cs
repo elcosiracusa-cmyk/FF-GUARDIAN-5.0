e56dcb49157406be61534b748bc1d11c44070103
elcosiracusa-cmyk <el.co.siracusa@gmail.com>
2026-07-29 11:31:54 +0200

    Enable FF GUARDIAN 6.0.1 readability layer

--- FFGuardian.App/Program.cs
+++ FFGuardian.App/Program.cs
@@ -29,6 +29,7 @@ internal static class Program
         Application.Idle += SupportEmailLayoutFix.Apply;
         Application.Idle += Advanced60Ui.Apply;
         Application.Idle += Version60Fix.Apply;
+        Application.Idle += UiReadabilityFix.Apply;
         Application.ThreadException += (_, e) =>
         {
             (string message, MessageBoxIcon icon) = ErrorMessageFormatter.Format(e.Exception);
