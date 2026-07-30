4b2f25a2412f2266ff9263d60e2da8d3ab0d9f7e
elcosiracusa-cmyk <el.co.siracusa@gmail.com>
2026-07-30 12:43:03 +0200

    Collegare Advanced Settings 8.1 all'avvio

--- FFGuardian.App/Program.cs
+++ FFGuardian.App/Program.cs
@@ -33,6 +33,7 @@ internal static class Program
         Application.Idle += ProfessionalSecurityCenter63.Apply;
         Application.Idle += SidebarFirewallFix631.Apply;
         Application.Idle += CloudReady80.Apply;
+        Application.Idle += AdvancedSettings81.Apply;
         Application.ThreadException += (_, e) =>
         {
             (string message, MessageBoxIcon icon) = ErrorMessageFormatter.Format(e.Exception);
