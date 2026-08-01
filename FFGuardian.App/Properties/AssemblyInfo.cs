b65968cb16dc73a9359bb2d5d9332b97999bb281
elcosiracusa-cmyk <el.co.siracusa@gmail.com>
2026-08-01 22:13:45 +0200

    test: expose Engine10 internals to autonomous agent tests

--- FFGuardian.App/Properties/AssemblyInfo.cs
+++ FFGuardian.App/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.SmokeTests")]
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.RemediationTests")]
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.UpdateTests")]
+[assembly: InternalsVisibleTo("FFGuardian.Engine10.AgentTests")]
