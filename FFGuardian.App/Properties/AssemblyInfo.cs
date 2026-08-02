f30227f7a778ff2e8049679ee9752d4be8215cb3
elcosiracusa-cmyk <el.co.siracusa@gmail.com>
2026-08-02 22:41:01 +0200

    test: abilita test dedicati alla reputazione

--- FFGuardian.App/Properties/AssemblyInfo.cs
+++ FFGuardian.App/Properties/AssemblyInfo.cs
@@ -4,3 +4,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.RemediationTests")]
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.UpdateTests")]
 [assembly: InternalsVisibleTo("FFGuardian.Engine10.AgentTests")]
+[assembly: InternalsVisibleTo("FFGuardian.Engine10.ReputationTests")]
